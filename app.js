bfe43a4136314c108057c1f22614fd9e17943970
Derek <derekdanielson84@gmail.com>
2023-03-06 13:05:02 -0700

    Commit

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
-/*
-document.querySelector('.clear-tasks').addEventListener('click', function(e){
-    console.log('Hello');
-
-    e.preventDefault();
+// EVENT BUBBLING -- calling card-title triggers each of this event listeners as it 'bubbles up' through the parents
+document.querySelector('.card-title').addEventListener('click', function(){
+    console.log('card title');
 });
-*/
+
+document.querySelector('.card-content').addEventListener('click', function(){
+    console.log('card content');
+});
+
+document.querySelector('.card').addEventListener('click', function(){
+    console.log('card');
+});
+
+document.querySelector('.col').addEventListener('click', function(){
+    console.log('col');
+});
 
 
-document.querySelector('.clear-tasks').addEventListener('click', onClick);
+// EVENT DELEGATION
+document.body.addEventListener('click', deleteItem);
 
-function onClick(e){
-    //console.log('Clicked');
-    let val;
-    val = e;
 
-    // Event target element
-    val = e.target;
-    val = e.target.id;
-    val = e.target.className;
-    val = e.target.classList;
-
-    e.target.innerText = 'Hello';
-
-    // Event type
-    val = e.type;
-
-    // Timestamp
-    val = e.timeStamp;
-
-    // Coordinates event relative to the window
-    val = e.clientY; //pixels from top to bottom
-    val = e.clientX; //pixels from left to right
-
-    // Coordinates event relative to the element
-    val = e.offsetY;
-    val = e.offsetX;
-
-    console.log(val);
+function deleteItem(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log('delete item');
+        e.target.parentElement.parentElement.remove();
+    }
 }
-
